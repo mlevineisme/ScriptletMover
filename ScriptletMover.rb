11d1d0a0c63e506770e921f37f254167d9eb2386
Mike Levine <mlevineisme@gmail.com>
2014-04-15 13:24:34 -0700

    Properly edits destination file security to ensure copies are made if destination files were 'read only'

--- ScriptletMover.rb
+++ ScriptletMover.rb
@@ -7,16 +7,21 @@ SOURCE_FOLDER = FileUtils.pwd
 puts "The source folder used to move the Scriptlet files from: #{SOURCE_FOLDER}"
 DIRECTORY_CONFIG = File.open("#{SOURCE_FOLDER}/#{DIRECTORY_CONFIG_NAME}")
 directory_paths = Array.new
-   DIRECTORY_CONFIG.each_line {|line| directory_paths << line.chomp}
+DIRECTORY_CONFIG.each_line {|line| directory_paths << line.chomp}
 
-# Gather the files list, removing the config file and script file
+# Gather the files list, removing the config, script, and GIT files
 files = Dir.glob("*.*")
 files.delete(DIRECTORY_CONFIG_NAME)
 files.delete(__FILE__.split("/").last)
+files.delete("README.md")
 
 # Copy the source files into the destination directories
 directory_paths.each {|path|
-   FileUtils.chmod("u=wrx,go=rx", path)
+   
+   # Give write access to the files, so they can be overwritten if "read only"
+   files.each {|file|
+      FileUtils.chmod("u=wrx,go=rx", "#{path}\\#{file}")}
+
+   # Copy the files
    FileUtils.cp [*files], path
-   puts "Files copied to: #{path}"
-}
+   puts "Files copied to: #{path}"}
