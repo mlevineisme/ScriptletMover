315680df1796b53b38db80f47ac90cdb43580bfe
Mike Levine <mlevineisme@gmail.com>
2014-04-15 15:14:24 -0700

    Changed the comment style, made the config file finding more robust by having to leverage __FILE__, renamed variables to be more self documenting, and added an overall comment.

--- ScriptletMover.rb
+++ ScriptletMover.rb
@@ -1,31 +1,28 @@
+#-- This program copies files in the current directory that include "Scriptlets" into the
+# folders specified in the .conf file of the same name
+
 require 'FileUtils'
 
-# This is a config value that may need to be changed
-DIRECTORY_CONFIG_NAME = "ScriptletMover.conf"
-GIT_FILE = "README.md"
-
+DIRECTORY_CONFIG_NAME = "#{File.basename(__FILE__, ".rb")}.conf"
 SOURCE_FOLDER = FileUtils.pwd
 puts "The source folder used to move the Scriptlet files from: #{SOURCE_FOLDER}"
-DIRECTORY_CONFIG = File.open("#{SOURCE_FOLDER}/#{DIRECTORY_CONFIG_NAME}")
+DIR_CONF_FILE = File.open("#{SOURCE_FOLDER}/#{DIRECTORY_CONFIG_NAME}")
 directory_paths = Array.new
-DIRECTORY_CONFIG.each_line {|line| directory_paths << line.chomp}
+DIR_CONF_FILE.each_line {|line| directory_paths << line.chomp}
 
-# Gather the files list, removing the config, script, and GIT files
-files = Dir.glob("*.*")
-files.delete(DIRECTORY_CONFIG_NAME)
-files.delete(__FILE__.split("/").last)
-files.delete(GIT_FILE) if files.include?(GIT_FILE)
+#-- Gather the files list
+filesToMove = Dir.glob("*Scriptlets.*")
 
-# Copy the source files into the destination directories
+#-- Copy the source files into the destination directories
 directory_paths.each {|path|
    
-   # Give write access to the files in team, so they can be overwritten if "read only"
-   files.each {|file|
+   #-- Give write access to the files in team, so "read only" files can be overwritten
+   filesToMove.each {|file|
       FileUtils.chmod("u=wrx,go=rx", "#{path}\\#{file}") if path.include?("team") and File.exist?("#{path}\\#{file}")}
 
-   # Copy the files to their new directory
+   #-- Copy the files to their new directory
    if (File.directory?(path))
-      FileUtils.cp [*files], path, :preserve => true
+      FileUtils.cp [*filesToMove], path, :preserve => true
       puts "Files copied to: #{path}"
    else
       puts "Files were not copied to #{path}, because the path was invalid"
