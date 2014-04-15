65801b624cecde823d0e0980a5fe80544512cf82
unknown <80100497@SDWMJDLYB8.Hospira.corp>
2014-04-15 13:02:07 -0700

    First working ScriptletMover, broken permission changes

---
+++ ScriptletMover.rb
@@ -0,0 +1,22 @@
+require 'FileUtils'
+
+# This is a config value that may need to be changed
+DIRECTORY_CONFIG_NAME = "ScriptletMover.conf"
+
+SOURCE_FOLDER = FileUtils.pwd
+puts "The source folder used to move the Scriptlet files from: #{SOURCE_FOLDER}"
+DIRECTORY_CONFIG = File.open("#{SOURCE_FOLDER}/#{DIRECTORY_CONFIG_NAME}")
+directory_paths = Array.new
+   DIRECTORY_CONFIG.each_line {|line| directory_paths << line.chomp}
+
+# Gather the files list, removing the config file and script file
+files = Dir.glob("*.*")
+files.delete(DIRECTORY_CONFIG_NAME)
+files.delete(__FILE__.split("/").last)
+
+# Copy the source files into the destination directories
+directory_paths.each {|path|
+   FileUtils.chmod("u=wrx,go=rx", path)
+   FileUtils.cp [*files], path
+   puts "Files copied to: #{path}"
+}
