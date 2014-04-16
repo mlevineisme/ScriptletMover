e952fb594613d22a8db7a31ca1437d0b6d51c861
Mike Levine <mlevineisme@gmail.com>
2014-04-16 10:18:51 -0700

    Parens for conditions

--- ScriptletMover.rb
+++ ScriptletMover.rb
@@ -37,7 +37,7 @@ directory_paths.each do |path|
    
    #-- Give write access to the files in team, so "read only" files can be overwritten
    filesToMove.each do |file|
-      FileUtils.chmod("u=wrx,go=rx", "#{path}\\#{file}") if path.include?("team") and File.exist?("#{path}\\#{file}") #
+      FileUtils.chmod("u=wrx,go=rx", "#{path}\\#{file}") if (path.include?("team") and File.exist?("#{path}\\#{file}"))
    end
 
    begin
@@ -49,7 +49,7 @@ directory_paths.each do |path|
          # verification that the files were correctly copied
          filesEqual = Array.new
          filesToMove.each do |file|
-            if File.mtime("#{path}\\#{file}").eql?(File.mtime(file))
+            if (File.mtime("#{path}\\#{file}").eql?(File.mtime(file)))
                filesEqual << true
             else
                filesEqual << false
